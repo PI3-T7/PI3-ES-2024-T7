0406db6afcf0964b1f35225183fc17bf32c570a3
Lais Lemos <laisaplemos10@gmail.com>
2024-03-10 18:57:04 -0300

    Imagens da tela de abertura adicionadas e atividade Opening criada

---
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/Opening.kt
@@ -0,0 +1,11 @@
+package br.edu.puccampinas.projeto_smart_locker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Opening : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opening)
+    }
+}
