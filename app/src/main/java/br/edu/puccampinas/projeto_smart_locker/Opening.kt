5c18aa34e29ccf26235c14c38005aa776d99149e
Lais Lemos <laisaplemos10@gmail.com>
2024-04-02 22:00:35 -0300

    Arrumei o direcionamento dos botoes da tela de abertura

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/Opening.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/Opening.kt
@@ -8,43 +8,43 @@ import android.widget.TextView
 
 /**
  * Esta classe representa a atividade de abertura do aplicativo.
- * Ela exibe um botão que leva à atividade de login e um TextView para cadastro.
+ * Ela exibe um botão que leva à atividade de cadastro e um TextView para login.
  */
 class Opening : AppCompatActivity() {
 
-    // Botão para iniciar a atividade de login
-    private lateinit var botaoComecar: Button
-    // EditText para iniciar a atividade de cadastro
-    private lateinit var etCadastro: TextView
+    // Botão para abrir a atividade de cadastro
+    private lateinit var botaoCadastro: Button
+    // TextView para abrir a atividade de login
+    private lateinit var tvLogin: TextView
 
     /**
      * Método chamado quando a atividade é criada.
-     * Configura o layout e define o comportamento dos botões de início e cadastro.
+     * Configura o layout e define o comportamento dos botões de cadastro e login.
      * @param savedInstanceState O estado da instância da atividade, se disponível.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening)
 
-        // Inicializa o botão de início
-        botaoComecar = findViewById(R.id.bt_begin)
+        // Inicializa o botão de cadastro
+        botaoCadastro = findViewById(R.id.bt_begin)
 
-        // Define o listener de clique para o botão de início
-        botaoComecar.setOnClickListener {
-            // Cria uma intenção para iniciar a atividade de login
-            val intent = Intent(this, Login_activity::class.java)
-            // Inicia a atividade de login
-            startActivity(intent)
-        }
-
-        // Inicializa o TextView para cadastro
-        etCadastro = findViewById(R.id.already)
-
-        // Define o listener de clique para o TextView de cadastro
-        etCadastro.setOnClickListener {
+        // Define o listener de clique para o botão de cadastro
+        botaoCadastro.setOnClickListener {
             // Cria uma intenção para iniciar a atividade de cadastro
             val intent = Intent(this, SignUpActivity::class.java)
             // Inicia a atividade de cadastro
+            startActivity(intent)
+        }
+
+        // Inicializa o TextView para login
+        tvLogin = findViewById(R.id.already)
+
+        // Define o listener de clique para o TextView de login
+        tvLogin.setOnClickListener {
+            // Cria uma intenção para iniciar a atividade de login
+            val intent = Intent(this, Login_activity::class.java)
+            // Inicia a atividade de login
             startActivity(intent)
         }
     }
