81fbdf6832726e6ccf1c4ce2fde36eb8e62600fb
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-04-04 21:25:49 -0300

    Adicionando a API do Google Maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("com.google.secrets_gradle_plugin") version "0.5"
 }
 
 android {
@@ -64,6 +65,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/MapActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/MapActivity.kt
@@ -2,10 +2,75 @@ package br.edu.puccampinas.projeto_smart_locker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapActivity : AppCompatActivity() {
+
+    private val places = arrayListOf(
+        Place(
+            "PUCCAMP",
+            LatLng(-22.8345605, -47.0635005),
+            "Ac. Publico, 286 - Parque dos Jacarandás, Campinas - SP, 13086-061",
+            4.8f
+        ),
+        Place(
+            "Shopping Dom Pedro",
+            LatLng(-22.8475613, -47.0677126),
+            "Av. Guilherme Campos, 500 - Jardim Santa Genebra, Campinas - SP, 13080-000",
+            4.7f
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+
+        mapFragment.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                places.forEach() {
+                    bounds.include(it.latLng)
+                }
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 200))
+            }
+        }
     }
+
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .snippet(place.address)
+                    .position(place.latLng)
+                    .icon( /*para mudar o icon*/
+                        BitmapHelper.vectorToBitmap(
+                            this, R.drawable.icon_pin_location,
+                            ContextCompat.getColor(this, R.color.orange)
+                        )
+                    )
+
+            )
+        }
+    }
+
 }
+
+data class Place(
+    val name: String,
+    val latLng: LatLng,
+    val address: String,
+    val rating: Float
+)
