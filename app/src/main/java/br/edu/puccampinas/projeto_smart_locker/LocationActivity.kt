8b765fb3bfbcc933979b2061fb0f00b900c49ca0
Lais Lemos <laisaplemos10@gmail.com>
2024-04-19 17:13:36 -0300

    Locação de armários quase finalizada

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/LocationActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/LocationActivity.kt
@@ -18,11 +18,15 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
+import kotlin.math.*
 
 class LocationActivity : AppCompatActivity() {
 
     // Variável que acessa os serviços de localização da API
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private var latitudeUser: Double = 0.0
+    private var longitudeUser: Double = 0.0
 
     companion object {
         // Código de solicitação de permissão
@@ -43,9 +47,16 @@ class LocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
 
+        btn30min = findViewById(R.id.btn30min)
+        btn1hour = findViewById(R.id.btn1hour)
+        btn2hours = findViewById(R.id.btn2hours)
+        btn4hours = findViewById(R.id.btn4hours)
+        btnUntil18 = findViewById(R.id.btnUntil18)
+        btnConfirmLocation = findViewById(R.id.bt_confirm_location)
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Verificar e solicitar permissão de acesso à localização
+        // Verifique e solicite permissão de acesso à localização
         if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -61,43 +72,26 @@ class LocationActivity : AppCompatActivity() {
             obterLocalizacaoAtual()
         }
 
-        btn30min = findViewById(R.id.btn30min)
-        btn1hour = findViewById(R.id.btn1hour)
-        btn2hours = findViewById(R.id.btn2hours)
-        btn4hours = findViewById(R.id.btn4hours)
-        btnUntil18 = findViewById(R.id.btnUntil18)
-        btnConfirmLocation = findViewById(R.id.bt_confirm_location)
-
-        // Obter a hora atual
-        val calendario = Calendar.getInstance()
-        val horaAtual = calendario.get(Calendar.HOUR_OF_DAY)
-
-        // Verificar se está entre 7 e 8 horas
-        if (horaAtual in 7..8) {
-            btnUntil18.visibility = View.VISIBLE
-        } else {
-            btnUntil18.visibility = View.GONE
-        }
 
         btnConfirmLocation.setOnClickListener {
             if (btn30min.isChecked) {
                 // Código para 30 minutos selecionado - cadastrar locação no banco
-                Toast.makeText(this, "30 minutos selecionado", Toast.LENGTH_SHORT).show()
+
             } else if (btn1hour.isChecked) {
                 // Código para 1 hora selecionada
-                Toast.makeText(this, "1 hora selecionada", Toast.LENGTH_SHORT).show()
+
             } else if (btn2hours.isChecked) {
                 // Código para 2 horas selecionadas
-                Toast.makeText(this, "2 horas selecionadas", Toast.LENGTH_SHORT).show()
+
             } else if (btn4hours.isChecked) {
                 // Código para 4 horas selecionadas
-                Toast.makeText(this, "4 horas selecionadas", Toast.LENGTH_SHORT).show()
+
             } else if (btnUntil18.isChecked) {
                 // Código para até 18 horas selecionadas
-                Toast.makeText(this, "Até 18 horas selecionadas", Toast.LENGTH_SHORT).show()
+
             } else {
                 // Nenhum botão de rádio selecionado
-                Toast.makeText(this, "Selecione uma opção de tempo", Toast.LENGTH_SHORT).show()
+
             }
         }
     }
@@ -110,7 +104,6 @@ class LocationActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permissão concedida, obter a localização atual
                 obterLocalizacaoAtual()
             } else {
                 // Permissão negada, mostrar mensagem ou tratar de outra forma
@@ -124,6 +117,7 @@ class LocationActivity : AppCompatActivity() {
     }
 
     private fun obterLocalizacaoAtual() {
+
         // Obter a última localização conhecida do usuário
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -133,27 +127,44 @@ class LocationActivity : AppCompatActivity() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            Toast.makeText(
+                this,
+                "Permissão não concedida.",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location: Location? ->
                 if (location != null) {
-                    // Utilize a localização aqui
-                    // Exemplo: val latitude = location.latitude
-                    //           val longitude = location.longitude
+
+                    latitudeUser = location.latitude
+                    longitudeUser = location.longitude
+
+                    Log.d(TAG, "Latitude do usuário: $latitudeUser")
+                    Log.d(TAG, "Longitude do usuário: $longitudeUser")
+
+                    carregarDadosBanco()
+
                 } else {
-                    // Localização não disponível
                     Toast.makeText(
                         this,
-                        "Não foi possível obter a localização atual.",
+                        "Não foi possível obter a localização.",
                         Toast.LENGTH_SHORT
                     ).show()
+
                 }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Não foi possível obter a localização atual.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun carregarDadosBanco() {
 
         val editLocal = findViewById<TextView>(R.id.local)
 
@@ -168,19 +179,65 @@ class LocationActivity : AppCompatActivity() {
                 if (document.exists()) {
                     // O documento com o ID "1" existe, agora você pode acessar seus dados
                     val nome = document.getString("name")
-                    editLocal.text = nome
 
-                    // Acessando o array de preços
-                    val pricesArray = document.get("prices") as? List<*>
-                    if (pricesArray != null) {
-                        // Limpar as opções anteriores
-                        clearRadioButtonOptions()
+                    // Acessando a latitude e longitude do ponto de locação
+                    val latLng = document.getGeoPoint("latLng")
 
-                        // Adicionar os preços aos RadioButtons
-                        addPricesToRadioButtons(pricesArray)
+
+                    if (latLng != null) {
+                        val latitude = latLng.latitude
+                        val longitude = latLng.longitude
+
+                        Log.d(TAG, "Latitude: $latitude")
+                        Log.d(TAG, "Longitude: $longitude")
+
+                        val distanciaLimite = 1 // distância limite em quilômetros
+
+                        val distancia = calcularDistancia(
+                            latitudeUser, longitudeUser, latitude, longitude
+                        )
+
+                        if (distancia <= distanciaLimite) {
+                            // A localização está próxima do ponto de locação
+
+                            editLocal.text = nome
+
+                            val pricesArray = document.get("prices") as? List<*>
+                            if (pricesArray != null) {
+                                // Limpar as opções anteriores
+                                clearRadioButtonOptions()
+                                // Adicionar os preços aos RadioButtons
+                                addPricesToRadioButtons(pricesArray)
+
+                            } else {
+                                // O campo 'prices' não é um array ou está vazio
+                                Toast.makeText(
+                                    this@LocationActivity,
+                                    "Não há preços associados com essa Unidade de Locação.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        } else {
+                            // A localização não está próxima do ponto de locação
+                            val toast = Toast.makeText(applicationContext, "\"Você não está próximo(a) de nenhuma Unidade SmartLocker. Esteja a pelo menos 100 metros de uma e tente novamente.\"", Toast.LENGTH_LONG)
+                            val view = layoutInflater.inflate(R.layout.custom_toast_layout, null)
+                            val text = view.findViewById<TextView>(R.id.text)
+                            text.text = "Você deve estar a pelo menos 100 metros do ponto de locação escolhido para alugar um armário!"
+                            toast.view = view
+                            toast.show()
+
+                            finish()
+                        }
+
                     } else {
-                        // O campo 'prices' não é um array ou está vazio
+                        Toast.makeText(
+                            this@LocationActivity,
+                            "Geopoint nulo.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
+
                 } else {
                     // O documento com o ID "1" não existe
                 }
@@ -189,7 +246,6 @@ class LocationActivity : AppCompatActivity() {
                 Log.e(TAG, "Error getting document", exception)
                 // Lidar com falhas na recuperação aqui
             }
-
     }
 
     private fun clearRadioButtonOptions() {
@@ -215,6 +271,34 @@ class LocationActivity : AppCompatActivity() {
                 // Não há preços suficientes
                 Toast.makeText(this, "Não há preços suficientes", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun calcularDistancia(
+        lat1: Double, lon1: Double, lat2: Double, lon2: Double
+    ): Double {
+        val r = 6371.0 // raio da Terra em km
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val lat1Rad = Math.toRadians(lat1)
+        val lat2Rad = Math.toRadians(lat2)
+
+        val a = sin(dLat / 2) * sin(dLat / 2) +
+                sin(dLon / 2) * sin(dLon / 2) * cos(lat1Rad) * cos(lat2Rad)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return r * c
+    }
+
+    private fun checkHour(){
+        // Obter a hora atual
+        val calendario = Calendar.getInstance()
+        val horaAtual = calendario.get(Calendar.HOUR_OF_DAY)
+
+        // Verificar se está entre 7 e 8 horas
+        if (horaAtual in 7..8) {
+            btnUntil18.visibility = View.VISIBLE
+        } else {
+            btnUntil18.visibility = View.GONE
         }
     }
 
