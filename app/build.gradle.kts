9e9df4f49b3d0668bca60c1288adf20045a158f0
Lais Lemos <129067125+laislemos801@users.noreply.github.com>
2024-04-26 12:59:25 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -65,18 +68,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // API gerador de QRcode
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-
-    // Gson
-    implementation("com.google.code.gson:gson:2.10.1")
-
     // https://github.com/santalu/maskara - biblioteca mascaras
     implementation("com.github.santalu:maskara:1.0.0")
-    // API gerador de QRcode
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    // Gson
-    implementation("com.google.code.gson:gson:2.10.1")
 
     // Configuração do Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
@@ -85,13 +78,8 @@ dependencies {
     // Configuração do Firestore Database
     implementation("com.google.firebase:firebase-firestore-ktx")
 
-    implementation ("com.google.maps:google-maps-services:0.18.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
-    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -99,10 +87,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.google.android.gms:play-services-cronet:18.0.1")
 }
