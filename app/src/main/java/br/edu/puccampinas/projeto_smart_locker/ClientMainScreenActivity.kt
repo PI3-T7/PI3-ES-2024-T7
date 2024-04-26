db05e392eebed549d46c45e945e098cf2dc983db
Lais Lemos <laisaplemos10@gmail.com>
2024-04-25 23:06:46 -0300

    Verificações finalizadas

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/ClientMainScreenActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/ClientMainScreenActivity.kt
@@ -1,5 +1,7 @@
 package br.edu.puccampinas.projeto_smart_locker
 
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +12,22 @@ import androidx.annotation.RequiresApi
 import com.google.firebase.auth.FirebaseAuth
 
 class ClientMainScreenActivity : AppCompatActivity() {
+
+    private val sharedPref = "Locacao"
+    private val qrCodeBitMapKey = "locacaoPendente"
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_main_screen)
+
+        // Verifica se há uma locação pendente
+        val prefs = getSharedPreferences(sharedPref, Context.MODE_PRIVATE)
+        val locacaoPendente = prefs.getBoolean(qrCodeBitMapKey, false)
+
+        if (locacaoPendente) {
+            // Se houver uma locação pendente, exibe o diálogo
+            showLocacaoPendenteDialog()
+        }
 
         val botaoAlugar = findViewById<View>(R.id.containerRent)
         val botaoVerPontos = findViewById<View>(R.id.containerMap)
@@ -38,4 +52,39 @@ class ClientMainScreenActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun showLocacaoPendenteDialog() {
+        // Aqui você cria e exibe o diálogo para avisar o usuário sobre a locação pendente
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Locação Pendente")
+            .setMessage("Você tem uma locação pendente. Deseja continuar?")
+            .setPositiveButton("Sim") { dialog, _ ->
+                // Ação a ser executada quando o usuário clicar em "Sim"
+                dialog.dismiss()
+                // Coloque aqui o código para continuar com a locação pendente
+                val intent = Intent(this, QRcodeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .setNegativeButton("Não") { dialog, _ ->
+                // Ação a ser executada quando o usuário clicar em "Não"
+                dialog.dismiss()
+                // Coloque aqui o código para cancelar a locação pendente
+                cancelLocacaoPendente()
+            }
+            .setCancelable(false) // Impede que o usuário feche o diálogo ao tocar fora dele
+            .create()
+
+        dialog.show()
+    }
+
+    private fun cancelLocacaoPendente() {
+        // Coloque aqui o código para cancelar a locação pendente
+        // Por exemplo, você pode remover o status de locação pendente das SharedPreferences
+        val prefs = getSharedPreferences(sharedPref, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putBoolean(qrCodeBitMapKey, false) // Define como false para cancelar a locação pendente
+        editor.apply()
+    }
+
 }
--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/QRcodeActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/QRcodeActivity.kt
@@ -1,6 +1,9 @@
 package br.edu.puccampinas.projeto_smart_locker
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,16 +11,24 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.ByteArrayOutputStream
+import android.util.Base64
+
 
 class QRcodeActivity : AppCompatActivity() {
 
     private lateinit var imgQRcode: ImageView
+    private lateinit var buttonHome2: ImageView
+    private lateinit var buttonVoltar2: ImageView
+    private var qrCodeBitmap: Bitmap? = null // Declaração da variável qrCodeBitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrcode)
 
         imgQRcode = findViewById(R.id.img_qr_code)
+        buttonHome2 = findViewById(R.id.buttonHome2)
+        buttonVoltar2 = findViewById(R.id.buttonVoltar2)
 
         // Obtém a string serializada da Intent
         val dados = intent.getStringExtra("dados")
@@ -26,7 +37,69 @@ class QRcodeActivity : AppCompatActivity() {
         if (dados != null) {
             generateQRCode(dados)
         }
+
+        buttonVoltar2.setOnClickListener {
+            val intent = Intent(this, LocationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonHome2.setOnClickListener {
+            val intent = Intent(this, ClientMainScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Defina o nome das SharedPreferences e a chave do QR code bitmap
+        val sharedPref = "Locacao"
+        val qrCodeBitMapKey = "qrCodeBitmap" // Corrigido para qrCodeBitmap
+
+        // Verifica se há um QR code bitmap para salvar
+        qrCodeBitmap?.let { bitmap ->
+            // Salva o QR code bitmap nas SharedPreferences
+            val prefs = getSharedPreferences(sharedPref, Context.MODE_PRIVATE)
+            val editor = prefs.edit()
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+            val byteArray = byteArrayOutputStream.toByteArray()
+            val encodedBitmap = Base64.encodeToString(byteArray, Base64.DEFAULT)
+            editor.putString(qrCodeBitMapKey, encodedBitmap)
+            editor.putBoolean("locacaoPendente", true) // Defina como true se houver uma locação pendente
+            editor.apply()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Defina o nome das SharedPreferences e as chaves corretas
+        val sharedPref = "Locacao"
+        val qrCodeBitMapKey = "qrCodeBitmap" // Chave para o bitmap do QR code
+        val locacaoPendenteKey = "locacaoPendente" // Chave para o status de locação pendente
+
+        // Recupera o QR code bitmap das SharedPreferences
+        val prefs = getSharedPreferences(sharedPref, Context.MODE_PRIVATE)
+        val encodedBitmap = prefs.getString(qrCodeBitMapKey, null)
+
+        // Recupera o status de locação pendente das SharedPreferences
+        val locacaoPendente = prefs.getBoolean(locacaoPendenteKey, false)
+
+        // Verifica se o QR code bitmap está disponível
+        if (encodedBitmap != null) {
+            // Decodifica o bitmap do Base64
+            val decodedByteArray = Base64.decode(encodedBitmap, Base64.DEFAULT)
+            val decodedBitmap = BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.size)
+
+            // Define o bitmap na ImageView
+            imgQRcode.setImageBitmap(decodedBitmap)
+            imgQRcode.visibility = ImageView.VISIBLE
+        }
+    }
+
 
     // Função que gera um QRcode
     private fun generateQRCode(text: String) {
@@ -37,6 +110,7 @@ class QRcodeActivity : AppCompatActivity() {
             val barcodeEncoder = BarcodeEncoder()
             bitMatrix = barcodeEncoder.encode(text, BarcodeFormat.QR_CODE, width, height)
             val bitmap = toBitmap(bitMatrix)
+            qrCodeBitmap = bitmap // Atribui o bitmap gerado à variável qrCodeBitmap
             imgQRcode.setImageBitmap(bitmap)
             imgQRcode.visibility = ImageView.VISIBLE
         } catch (e: WriterException) {
@@ -56,6 +130,4 @@ class QRcodeActivity : AppCompatActivity() {
         }
         return bmp
     }
-
 }
-
