9a84f348567bb08f9003b0e6d4330eed0a07931e
Marcos do Amaral Miotto <129965433+marcos-do-amaral-miotto@users.noreply.github.com>
2024-04-26 21:52:33 -0300

    Cadastro de senha ativo no Banco de Dados

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/SignUpActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/SignUpActivity.kt
@@ -251,7 +251,6 @@ class SignUpActivity : AppCompatActivity() {
             values["email"].toString(), values["senha"].toString()
         ).addOnSuccessListener { authResult ->
             authResult.user?.sendEmailVerification()?.addOnCompleteListener {
-                values.remove("senha")
                 values.remove("senha2")
                 database.collection("Pessoas").document(authResult.user?.uid.toString()).set(values)
                 startActivity(Intent(this, VerifyActivity::class.java))
