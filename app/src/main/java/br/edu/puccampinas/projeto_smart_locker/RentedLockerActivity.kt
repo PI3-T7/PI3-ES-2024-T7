cc7892e63dd9d09d002e79eb773619b99e7d688c
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-05-02 08:43:28 -0300

    Adicionando RentedLockerActivity

---
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/RentedLockerActivity.kt
@@ -0,0 +1,20 @@
+package br.edu.puccampinas.projeto_smart_locker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RentedLockerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_rented_locker)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
