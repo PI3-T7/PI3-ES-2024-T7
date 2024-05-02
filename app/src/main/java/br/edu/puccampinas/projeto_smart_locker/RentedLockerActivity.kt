dbd1694d3dc567f6358e03285067da61f639066d
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-05-02 09:06:36 -0300

    Alterando activity RentedLocker

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/RentedLockerActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/RentedLockerActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.puccampinas.projeto_smart_locker.databinding.ActivityRentedLockerBinding
 
 class RentedLockerActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityRentedLockerBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_rented_locker)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
     }
 }
