a1ef01c672522805b9e6e6f3c4e91efc85669781
alexinsel <alexinsel@hotmail.com>
2024-05-03 15:06:11 -0300

    Telas gerente liberar armário

---
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/PersonPicActivity.kt
@@ -0,0 +1,29 @@
+package br.edu.puccampinas.projeto_smart_locker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.puccampinas.projeto_smart_locker.databinding.ActivityPersonPicBinding
+
+class PersonPicActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityPersonPicBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPersonPicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Listener para o botão de finalizar, redirecionando para a tela seguinte
+        binding.buttonFinish.setOnClickListener {
+            val intent = Intent(this, ClosetReleasedActivity::class.java)
+            startActivity(intent)
+        }
+        // Listener para seta de voltar, redirecionando o usuário a tela anterior
+        binding.imgArrow.setOnClickListener {
+            val intent = Intent(this, SelectPeopleNumActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/QRcodeManagerActivity.kt
@@ -0,0 +1,14 @@
+package br.edu.puccampinas.projeto_smart_locker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QRcodeManagerActivity: AppCompatActivity {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode_manager)
+
+
+
+}
