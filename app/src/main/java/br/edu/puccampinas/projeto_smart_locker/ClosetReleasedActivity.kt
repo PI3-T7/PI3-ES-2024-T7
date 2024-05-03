fc8b370f695d75deb11032704b4383471e7ddb23
alexinsel <alexinsel@hotmail.com>
2024-05-03 15:17:22 -0300

    Erro de syntax

--- app/src/main/java/br/edu/puccampinas/projeto_smart_locker/ClosetReleasedActivity.kt
+++ app/src/main/java/br/edu/puccampinas/projeto_smart_locker/ClosetReleasedActivity.kt
@@ -3,10 +3,10 @@ package br.edu.puccampinas.projeto_smart_locker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ClosetReleasedActivity: AppCompatActivity {
+class ClosetReleasedActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_closet_released)
-
-    }a
+    }
+}
