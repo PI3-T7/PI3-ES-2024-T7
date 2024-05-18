033ec35dbabbc195266588749cc059701c77602a
Lais Lemos <laisaplemos10@gmail.com>
2024-05-18 15:48:29 -0300

    Gradle do alex adicionado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,54 +53,68 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // API gerador de QRcode
+    // QRcode Scanner
+    implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
     // Gson
     implementation("com.google.code.gson:gson:2.10.1")
 
-    // API QRcode Scanner - https://github.com/yuriy-budiyev/code-scanner
-    implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
-
-    // https://github.com/santalu/maskara - biblioteca mascaras
+    // Maskara
     implementation("com.github.santalu:maskara:1.0.0")
 
-    // Configuração do Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
-    // Configuração do Authentication
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-auth")
-    // Configuração do Firestore Database
     implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
 
-    implementation ("com.google.maps:google-maps-services:0.18.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
+    // Google Maps
+    implementation("com.google.maps:google-maps-services:0.18.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
-    implementation("androidx.activity:activity:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // OkHttp
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.google.android.gms:play-services-cronet:18.0.1")
+
+    // Play Services Cronet
+    implementation("com.google.android.gms:play-services-cronet:18.0.1")
+
+    // Camera
+    implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation("androidx.camera:camera-core:1.3.3")
+    implementation("androidx.camera:camera-lifecycle:1.3.3")
+    implementation("androidx.camera:camera-view:1.3.3")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
